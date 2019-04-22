a52ead9fc33d181c61914ccf7469f0b8f24fdb13
Ubuntu <ubuntu@ip-10-0-0-133.ec2.internal>
2019-04-22 23:28:25 +0000

    Resolves issue with AWS version of mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+	// This should resolve the error Specified key was too long error
+        //   from https://laravel-news.com/laravel-5-4-key-too-long-error
+        Schema::defaultStringLength(191);
     }
 }
