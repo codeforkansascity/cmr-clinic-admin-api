be3cd8793cf1c38306159badfaaca195854bd26d
Paul Barham <paulb@pdblaptop.local>
2019-04-22 23:49:37 -0500

    Corrected spelling of judicial_circuit_number

---
+++ database/migrations/2019_04_23_043216_update_client_apr22.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateClientApr22 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('client', function (Blueprint $table) {
+            $table->renameColumn('judicial_ciruit_number', 'judicial_circuit_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('client', function (Blueprint $table) {
+            $table->renameColumn('judicial_circuit_number', 'judicial_ciruit_number');
+        });
+    }
+}
