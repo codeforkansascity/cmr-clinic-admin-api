c97d586b719c11883552c758afd1b577a3507ca5
Paul Barham <paulb@pdblaptop.local>
2019-04-26 00:51:21 -0500

    Increased size of charge in the charge table from 64 to 255

---
+++ database/migrations/2019_04_26_052927_increase_size_of_charges_charge_field.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IncreaseSizeOfChargesChargeField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('charge', function (Blueprint $table) {
+            $table->string('charge',255)->nullable()->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('charge', function (Blueprint $table) {
+            $table->string('charge', 64)->nullable()->change();
+
+        });
+    }
+}
