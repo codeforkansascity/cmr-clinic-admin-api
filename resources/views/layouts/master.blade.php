68664b1edf24c16dddd844ebdbc3dc2968385e07
Paul Barham <paulb@pdblaptop.local>
2019-07-05 00:14:10 -0500

    Cleaned up client index page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,83 +1,84 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('img/favicon.png') }}">
+        <link href="/css/app.css" rel="stylesheet">
+        <title>@yield('page-title') - APS</title>
+    </head>
+    <body class="body-with-nav">
+        <a href="#app" class="skip-link sr-only sr-only-focusable">Skip to main content</a>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- "Loading" overlay (off at page load) -->
+        <div class="loading-overlay global"></div>
+        <div class="loading-message global">
+            <!-- <div><img src="{{ asset('img/loading-icon.gif') }}" alt="Loading icon"> Loading</div> -->
+        </div>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Header/menu -->
+        @include('includes.nav')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        <main id="app" class="container" tabindex="-1">
+            <!-- Help link, if available -->
+            {{--@if($__env->yieldContent('page-help-link'))--}}
+                {{--<div class="page-help-link">--}}
+                    {{--<a href="@yield('page-help-link')" onclick="return confirm('TODO: modal? link?')">--}}
+                        {{--<img src="{{ asset('img/icons/help.svg') }}" class="svg-icon svg-icon-help" alt="Help icon" role="presentation">Help--}}
+                    {{--</a>--}}
+                {{--</div>--}}
+            {{--@endif--}}
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a href="/changePassword">Change Password</a><br>
-
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-
-                        @endguest
-                    </ul>
-                </div>
+            <!-- Page title and breadcrumbs -->
+            <div class="page-header">
+                @if($__env->yieldContent('page-header-title'))
+                    <h1 class="mb-0">@yield('page-header-title')</h1>
+                @endif
+                @if($__env->yieldContent('page-header-breadcrumbs'))
+                    <nav class="breadcrumb-wrap mt-1" aria-label="breadcrumb">
+                        <img src="{{ asset('img/icons/square.svg') }}" class="svg-icon svg-icon-square d-none d-lg-inline-block d-xl-inline-block" alt="Breadcrumb icon" role="presentation">
+                        @yield('page-header-breadcrumbs')
+                    </nav>
+                @endif
             </div>
-        </nav>
 
-        <main class="py-4">
+            <!-- Session message(s) (global) -->
+            <div class="alerts global mb-5">
+                @if ( \Session::has('flash_error_message'))
+                    <div class="alert alert-danger" role="alert">
+                        <img src="{{ asset('img/icons/danger.svg') }}" class="svg-icon svg-icon-danger svg-icon-error d-none d-lg-inline-block d-xl-inline-block" alt="Alert" title="Alert" role="presentation">
+                        {{ \Session::get('flash_error_message') }}
+                    </div>
+                @endif
+                @if ( \Session::has('flash_info_message'))
+                    <div class="alert alert-warning" role="alert">
+                        <img src="{{ asset('img/icons/warning.svg') }}" class="svg-icon svg-icon-warning d-none d-lg-inline-block d-xl-inline-block" alt="Info" title="Info" role="presentation">
+                        {{ \Session::get('flash_info_message') }}
+                    </div>
+                @endif
+                @if ( \Session::has('flash_success_message'))
+                    <div class="alert alert-success" role="alert">
+                        <img src="{{ asset('img/icons/success.svg') }}" class="svg-icon svg-icon-success d-none d-lg-inline-block d-xl-inline-block" alt="Checkmark" title="Success" role="presentation">
+                        {{ \Session::get('flash_success_message') }}
+                    </div>
+                @endif
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        <img src="{{ asset('img/icons/success.svg') }}" class="svg-icon svg-icon-success d-none d-lg-inline-block d-xl-inline-block" alt="Checkmark" title="Success" role="presentation">
+                        {{ session('status') }}
+                    </div>
+                @endif
+            </div>
+
+            <!-- Page content -->
             @yield('content')
-        </main>
-    </div>
-</body>
+        </main><!-- /.container -->
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="{{ mix('js/app.js') }}"></script>
+        @yield('scripts')
+    </body>
 </html>
