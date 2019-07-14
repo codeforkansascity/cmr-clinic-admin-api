493f50e9b1b734e8fccb704dfe0867abc3c391c9
Paul Barham <paulb@pdblaptop.local>
2019-07-14 15:38:32 -0500

    Charge fields convicted, eligible, and please_expunge are not boolean.

--- app/Lib/SetYesNoLib.php
+++ app/Lib/SetYesNoLib.php
@@ -8,12 +8,40 @@
 
 namespace App\Lib;
 
+use App\Charge;
+
 
 class SetYesNoLib
 {
 
     function __construct() {
-        print "Hello\n";
+        $charges = Charge::all();
+
+        foreach ($charges AS $charge) {
+            $charge->convicted = $this->convertBoolean($charge->convicted_text);
+            $charge->eligible = $this->convertBoolean($charge->eligible_text);
+            $charge->please_expunge = $this->convertBoolean($charge->please_expunge_text);
+            $charge->save();
+        }
+    }
+
+    function convertBoolean($val) {
+        switch ($val) {
+            case "1":
+                $val = true;
+                break;
+
+            case "0":
+                $val = false;
+                break;
+
+            default:
+                $val = null;
+                break;
+        }
+
+        return $val;
+
     }
 
 }
--- database/migrations/2019_07_14_194032_set-yes-no-migration.php
+++ database/migrations/2019_07_14_194032_set-yes-no-migration.php
@@ -14,6 +14,20 @@ class SetYesNoMigration extends Migration
      */
     public function up()
     {
+
+        Schema::table('charges', function (Blueprint $table) {
+            $table->renameColumn('convicted', 'convicted_text');
+            $table->renameColumn('eligible', 'eligible_text');
+            $table->renameColumn('please_expunge', 'please_expunge_text');
+
+        });
+        Schema::table('charges', function (Blueprint $table) {
+            $table->boolean('convicted')->nullable();
+            $table->boolean('eligible')->nullable();
+            $table->boolean('please_expunge')->nullable();
+
+        });
+
         $a = new SetYesNoLib();
     }
 
@@ -24,6 +38,15 @@ class SetYesNoMigration extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('charges', function (Blueprint $table) {
+            $table->dropColumn('convicted');
+            $table->dropColumn('eligible');
+            $table->dropColumn('please_expunge');
+        });
+        Schema::table('charges', function (Blueprint $table) {
+            $table->renameColumn('convicted_text', 'convicted');
+            $table->renameColumn('eligible_text', 'eligible');
+            $table->renameColumn('please_expunge_text', 'please_expunge');
+        });
     }
 }
