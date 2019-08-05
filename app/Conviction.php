0b23c806e499cc05987681b97012396eb066e3de
Paul Barham <paulb@pdblaptop.local>
2019-08-04 19:40:19 -0500

    Verified that application models has SoftDelete and RecordSignature

--- app/Conviction.php
+++ app/Conviction.php
@@ -3,11 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\RecordSignature;
 
 class Conviction extends Model
 {
 
+    use SoftDeletes;
     use RecordSignature;
 
     /**
