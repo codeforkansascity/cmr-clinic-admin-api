c0d1aaf462a1929c69702d16a8e1c786928c1b9f
matt <matthewjohncoleman@gmail.com>
2019-08-19 20:24:28 -0500

    add documentation for event bus

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,17 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+
+/**
+ *    Global Event Bus
+ *      -- Usage --
+ *
+ * Registering an event from a component
+ * this.$bus.$emit('event-name', value)
+ *
+ * Registering Listener
+ * this.$bus.$on('event-name', (value) => { // Your code here })
+*/
 Vue.prototype.$bus = new Vue();
 
 
