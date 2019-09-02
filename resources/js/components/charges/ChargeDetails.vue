497c22071a38d97e45d6aa9a154b3fbc046cc765
Paul Barham <paulb@pdblaptop.local>
2019-09-01 22:38:16 -0400

    Cleaned up charges detail

--- resources/js/components/charges/ChargeDetails.vue
+++ resources/js/components/charges/ChargeDetails.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div style="margin-bottom: 1em">
         <div class="row" >
 
             <div class="col-md-6">
@@ -24,74 +24,10 @@
             <div class="col-md-6">
                 <div class="form-group row mb-2 mb-md-0 text-only">
                     <label class="col-md-4 col-form-label text-md-right">
-                        Conviction Id
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.conviction_id"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Charge
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.charge"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Citation
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.citation"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Conviction Class Type
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.conviction_class_type"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Conviction Charge Type
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.conviction_charge_type"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
                         Sentence
                     </label>
                     <div class="col-md-8">
                         <dsp-text v-model="charge.sentence"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Convicted Text
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.convicted_text"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Eligible Text
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.eligible_text"/>
-                    </div>
-                </div>
-                <div class="form-group row mb-2 mb-md-0 text-only">
-                    <label class="col-md-4 col-form-label text-md-right">
-                        Please Expunge Text
-                    </label>
-                    <div class="col-md-8">
-                        <dsp-text v-model="charge.please_expunge_text"/>
                     </div>
                 </div>
                 <div class="form-group row mb-2 mb-md-0 text-only">
