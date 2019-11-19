de5ce7360a651a8d42dc5561c09cb0cddfba533e
matt <matthewjohncoleman@gmail.com>
2019-11-18 23:13:45 -0600

    sort cases

--- resources/js/components/cases/CasesList.vue
+++ resources/js/components/cases/CasesList.vue
@@ -44,8 +44,12 @@
             Object.keys(this.data).forEach(i =>
                 this.$set(this.cases, i, this.data[i])
             );
+            this.sortCases()
+
+
             this.$bus.$on('case-deleted', (case_id) => {
                 this.removeCase(case_id)
+                this.sortCases()
             });
         },
         data() {
@@ -96,8 +100,76 @@
                 console.log('updateCase=' + key);
                 console.log(value);
                 this.cases[key] = value
+                this.sortCases()
+            },
+
+            sortCases() {
+                let sorted = this.cases.sort((a,b) => {
+                    let a_date = this.findFirstValidDate(a)
+
+                    let b_date = this.findFirstValidDate(b)
+
+                    return a_date < b_date? 1 : -1
+                })
+                this.cases = []
+                this.$nextTick(() => {
+                    this.cases = sorted
+                })
+            },
+
+            findFirstValidDate(obj) {
+
+                let fields = ['release_date', 'date_of_disposition', 'date_of_charge', 'arrest_date']
+                for(let v of fields.values()) {
+                    let d = this.dateParse(obj[v])
+                    if(d) {
+                        return d
+                    }
+                }
+
+                return null
+            },
+
+            dateParse(date) {
+                if(!date) return null
+
+                /// replace all - with /
+                date = date.split('-').join( '/')
+
+                if(date.match(/^\d{2}\/\d{2}\/\d{4}$/)) {
+                    // mm/dd/yyyy
+                    return new Date(date)
+
+                } else if(date.match(/^\d{4}\/\d{2}\/\d{2}$/)) {
+                    // yyyy/mm/dd
+                    let [y, m, d] = date.split('/')
+                    return new Date(y, m -1, d)
+
+                } else if (date.match(/\d{2}\/\d{2}\/\d{2}$/)) {
+                    // mm/dd/yy
+                    let [m , d, y] = date.split('/');
+                    y = y > 20? y+2000 : y+1900
+                    return new Date(y, m-1, d)
+
+                } else if(date.match(/^\d{2}\/\d{2,4}$/) && date.length <= 7) {
+                    // mm/yy or mm/yyyy
+                    let[m, y] = date.split('/')
+                    if(y < 1000) {
+                        y = y > 20? y+2000 : y+1900
+                    }
+                    return new Date(y, m-1)
+
+                } else if(date.match(/^\d{4}$/)) {
+                    // YYYY year only
+                    return new Date(date)
+
+                }
+
+                return null
             }
+
         },
+
     }
 </script>
 
