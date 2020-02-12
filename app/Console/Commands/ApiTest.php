87426a8b744f43488002be355e736cf5b174789d
Paul Barham <paulb@pdblaptop.local>
2020-02-11 21:40:11 -0500

    Added api-test  command and updated wiki

---
+++ app/Console/Commands/ApiTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class ApiTest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cms:api-test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Display user info for API client';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $accessToken = env('TEST_API_TOKEN');
+        $url = env('APP_URL');
+
+        $client = new Client([                                     // GuzzleHttp\Client;
+            'base_uri' => "$url/api/",
+            'verify' => false,                                  // only for http:  remove for https:
+        ]);
+
+        $response = $client->request('GET', 'user', [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Bearer '.$accessToken,
+                ],
+            ]
+        );
+        echo $response->getBody();
+    }
+}
