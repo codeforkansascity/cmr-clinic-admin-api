cc8c753f92a11c8691aa12a64505a6629ce3aef2
Paul Barham <paulb@savagesoft.com>
2021-10-29 14:26:32 -0500

    Added usage message to first page/dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Use of this site</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-
-
-
+                    <h2></h2>
+                    <ul>
+                        <li>You understand that data on this site has not been validated.
+                        It is expected that you will use it as a starting point of your research.</li>
+                        <li>If you find an error or have a suggestion, you will report it by emailing paulb@savagesoft.com</li>
+                    </ul>
                 </div>
             </div>
         </div>
